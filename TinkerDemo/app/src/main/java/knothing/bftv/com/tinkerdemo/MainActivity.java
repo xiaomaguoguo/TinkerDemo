2a8f97a354d6a6d62db1819b3c4e73511bad10e8
mazhihua <mazhihua@bftv.com>
2017-05-26 13:24:37 +0800

    DEMO终于成功了,擦嘞 ！！！！

--- TinkerDemo/app/src/main/java/knothing/bftv/com/tinkerdemo/MainActivity.java
+++ TinkerDemo/app/src/main/java/knothing/bftv/com/tinkerdemo/MainActivity.java
@@ -1,13 +1,50 @@
 package knothing.bftv.com.tinkerdemo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button button,button2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.button: // 加载
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk";
+                File file = new File(path);
+                if(file.exists()){
+                    TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),path);
+                }
+                break;
+
+            case R.id.button2: //测试
+                Toast.makeText(this, "from Patch", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
--- TinkerDemo/app/src/main/java/knothing/bftv/com/tinkerdemo/MyApplication.java
+++ TinkerDemo/app/src/main/java/knothing/bftv/com/tinkerdemo/MyApplication.java
@@ -1,8 +1,67 @@
 package knothing.bftv.com.tinkerdemo;
 
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+import knothing.bftv.com.tinkerdemo.Log.MyLogImp;
+import knothing.bftv.com.tinkerdemo.util.SampleApplicationContext;
+import knothing.bftv.com.tinkerdemo.util.TinkerManager;
+
 /**
  * Created by MaZhihua on 2017/5/26.
  */
 
-public class MyApplication {
+@DefaultLifeCycle(application = "knothing.bftv.com.tinkerdemo.SampleApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class MyApplication extends DefaultApplicationLike{
+
+    public MyApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+//        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
 }
